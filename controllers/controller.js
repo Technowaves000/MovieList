38e513e8a7bc8dfb9ffaef84f71b8d6edbc9eefa
shanicka-minor <67990192+shanicka-minor@users.noreply.github.com>
2021-02-10 22:25:17 +0800

    fixed getReview form

--- controllers/controller.js
+++ controllers/controller.js
@@ -77,8 +77,8 @@ const controller = {
     let err;
     let {username, password} = req.body;
     // add to db code here
-    
-  
+
+
     // db.insertOne(userModel, user, function(flag){});
 
     if (!username || !password) {
@@ -113,7 +113,7 @@ const controller = {
     }
     console.log(username + "= username")
     console.log(password + "= password")
-    
+
   },
 
   getHome: function(req, res) {
@@ -130,21 +130,28 @@ const controller = {
   },
 
   getReview: function (req, res) {
-    if(req.params.filmid == 001) {
-      res.render('review', {url:  'https://images-na.ssl-images-amazon.com/images/I/41tlg4w9XdL._AC_.jpg'})
-    }
-    else if (req.params.filmid == 002) {
-      res.render('review', {url:  'https://i.pinimg.com/originals/49/0f/8a/490f8a2f9e8aaf38db71aa8b906e1908.jpg'})
-    }
-    else if (req.params.filmid == 003) {
-      res.render('review', {url:  'https://www.joblo.com/assets/images/joblo/posters/2020/03/76AA8E0F-AF7A-4FC2-A7DF-C194DBC85900.jpeg'})
-    }
-    else if (req.params.filmid == 004) {
-      res.render('review', {url:  'https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg'})
-    }
-    else {
-      res.send('404 NOT FOUND')
-    }
+    var id = req.params.filmid;
+
+    db.findOne(movieModel, { _id: id }, {}, function(result){
+        res.render('review', {review:result});
+    })
+
+    // res.render('review', req.params.filmid)
+    // if(req.params.filmid == 001) {
+    //   res.render('review', {url:  'https://images-na.ssl-images-amazon.com/images/I/41tlg4w9XdL._AC_.jpg'})
+    // }
+    // else if (req.params.filmid == 002) {
+    //   res.render('review', {url:  'https://i.pinimg.com/originals/49/0f/8a/490f8a2f9e8aaf38db71aa8b906e1908.jpg'})
+    // }
+    // else if (req.params.filmid == 003) {
+    //   res.render('review', {url:  'https://www.joblo.com/assets/images/joblo/posters/2020/03/76AA8E0F-AF7A-4FC2-A7DF-C194DBC85900.jpeg'})
+    // }
+    // else if (req.params.filmid == 004) {
+    //   res.render('review', {url:  'https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg'})
+    // }
+    // else {
+    //   res.send('404 NOT FOUND')
+    // }
   },
 
   postReview: async function (req, res) {
