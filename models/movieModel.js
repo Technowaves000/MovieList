a3c56315ce89804e04119bad6fba327d7d1fe516
shanicka-minor <67990192+shanicka-minor@users.noreply.github.com>
2021-02-11 13:37:33 +0800

    additional properities for moviemodel
    rating is set to 0 as default. numRaters is added to log the number of users who rate the movie. "Review: [Reviews]" is uncommented cos error pag hindi

--- models/movieModel.js
+++ models/movieModel.js
@@ -6,8 +6,9 @@ const modelSchema = new mongoose.Schema({
     Title: {type: String, required: true},
     Year: {type: Number, required: true},
     Description: {type: String, required: true},
-    Rating: {type: Number, required: true},
-    // Review: [Reviews],
+    Rating: {type: Number, required: true, default: 0},
+    numRaters: {type: Number, default: 0},
+    Review: [Reviews],
     Picture: {type: String, required: true}
 })
 
